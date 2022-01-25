4ce8c65825f599202581f0f8e43df7a845b3ecf4
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 18:59:36 +0100

    feat (view button): sdd button ipv4 and website

--- src/view.py
+++ src/view.py
@@ -11,13 +11,34 @@ def simple_view(input: str):
         view.add_item(item=offline)
         return view
     
-def website_view(input: str, domaine: str = None):
+def v4_view(input: str, domain: str = None):
     view = discord.ui.View()
     if input == "online":    
         online = discord.ui.Button(style=discord.ButtonStyle.green, label="Online")
+        website = discord.ui.Button(style=discord.ButtonStyle.green, label=f"{domain}", url=f"https://{domain}")
         view.add_item(item=online)
+        view.add_item(item=website)
         return view
     if input == "offline":    
         offline = discord.ui.Button(style=discord.ButtonStyle.red, label="Offline")
+        website = discord.ui.Button(style=discord.ButtonStyle.green, label=f"{domain}", url=f"https://{domain}")
         view.add_item(item=offline)
+        view.add_item(item=website)
         return view
+    
+def v6_view(input: str, domain: str = None):
+    view = discord.ui.View()
+    if input == "online":    
+        online = discord.ui.Button(style=discord.ButtonStyle.green, label="Online")
+        website = discord.ui.Button(style=discord.ButtonStyle.green, label=f"{domain}", url=f"https://[{domain}]")
+        view.add_item(item=online)
+        view.add_item(item=website)
+        return view
+    if input == "offline":    
+        offline = discord.ui.Button(style=discord.ButtonStyle.red, label="Offline")
+        website = discord.ui.Button(style=discord.ButtonStyle.green, label=f"{domain}", url=f"https://[{domain}]")
+        view.add_item(item=offline)
+        view.add_item(item=website)
+        return view
+    
+        
