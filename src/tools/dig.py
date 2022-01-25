6e7ab8bc573bab5bb338d5eb784af4b70318e9c0
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 18:09:54 +0100

    feat (dig subprocess): add async + embed function

--- src/tools/dig.py
+++ src/tools/dig.py
@@ -1,45 +1,39 @@
-import discord
-import subprocess
+from src.process import *
+from src.error import *
+from src.embed import *
+from src.view import *
 
 async def dig (ctx,args):
     
-    if len(args)==0 or len(args) >1:
+    if len(args) != 1:
         print("Missing Required Argument")
-        embed=discord.Embed(title="Error", description = "Missing Required Argument. \n \n You should use the **dig** command with IPv4 or IPv6 like this : \n \n IPv4:\n`.dig 1.1.1.1`\n IPv6: \n`.dig 2606:4700:4700::1111`",color=0xFF0000)
-        embed.set_thumbnail(url="https://discord.bots.gg/img/logo_transparent.png")
-        await ctx.channel.send(embed=embed)
+        await ctx.channel.send(embed=CompleteEmbed("Error", "Missing Required Argument. \n \n You should use the **dig** command with IPv4 or IPv6 like this : \n \n IPv4:\n`.dig 1.1.1.1`\n IPv6: \n`.dig 2606:4700:4700::1111`", 0xFF0000))
         return
     
     ip=args[0]
     print("Dig for", ip, "request by", ctx.author.name,f"(ID = {ctx.author.id}).")
-    domaine=str(ip)
 
-    view = discord.ui.View()    
-    offline = discord.ui.Button(style=discord.ButtonStyle.red, label="Offline")
-    online = discord.ui.Button(style=discord.ButtonStyle.green, label="Online")
-    
-
-    waiting1 = ("Dig " + domaine)
-    await ctx.channel.send(f"```py\n{waiting1} in progress ...```")
+    await ctx.channel.send(embed=CompleteEmbed("Dig", f"Dig **{ip}** in progress ...", 0x00FF00))
+    msg2= await ctx.channel.send(":hourglass:")
         
     try:
-        p = subprocess.run(["dig", "+short", "-x", domaine],capture_output=True, text=True, timeout=10)
-        
+        res,err=await execute_prog(f"dig +short -x {ip}", 10)
 
-    except subprocess.TimeoutExpired:
+    except TimeoutError:
         print("Timeout")
-        view.add_item(item=offline)
-        await ctx.channel.send(f"```py\nTimeout for {domaine}```", view=view)
+        await ctx.channel.send(embed=CompleteEmbed("Timeout", f"➜ Timeout for **{ip}**", 0xFF0000), view=simple_view("offline"))
         return
-
-   
-    res = str(p.stdout)
-    no_entry = f"Host {domaine} not found."
-    
-
-    if len(res) != 0:
-        view.add_item(item=online)
-        await ctx.channel.send(f"```py\n{res}```", view=view)
+    except ErrorDuringProcess as err:
+        print(f"Error code: {err.code}")
+        await msg2.delete()
+        await ctx.channel.send(embed=CompleteEmbed(f"Error {err.code}",f"➜ Dig **{ip}** not found.",0xFF0000), view=simple_view("offline"))
+        return
+        
     else:
-        view.add_item(item=offline)
-        await ctx.channel.send(f"```py\n{no_entry}```", view=view)
+        if len(res) != 0:
+            await msg2.edit(f"```py\n{res}```", view=simple_view("online"))
+            return
+        else:
+            await msg2.delete()
+            await ctx.channel.send(embed=CompleteEmbed("Error",f"➜ Dig **{ip}** not found.",0xFF0000), view=simple_view("offline"))
+            return
