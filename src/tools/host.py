b2698cd1c1164468f8e26a5c5f31faae8feaccef
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 17:35:02 +0100

    fix (host embed): use embed function

--- src/tools/host.py
+++ src/tools/host.py
@@ -1,26 +1,20 @@
 import discord
 from src.process import *
 from src.error import *
+from src.embed import *
+from src.view import *
 
 async def host (ctx,args):
     
     if len(args) != 1 :
         print("Missing Required Argument")
-        embed=discord.Embed(title="Error", description = "Missing Required Argument. \n \n You should use the **host** command with domain like this : \n \n Domain:\n`.host google.com`",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        await ctx.channel.send(embed=embed)
+        await ctx.channel.send(embed=CompleteEmbed("Error", "Missing Required Argument. \n \n You should use the **host** command with domain like this : \n \n Domain:\n`.host google.com`", 0xFF0000))
         return
     
     ip=args[0]
     print("Host for", ip, "request by", ctx.author.name,f"(ID = {ctx.author.id}).")
     
-    view = discord.ui.View()    
-    offline = discord.ui.Button(style=discord.ButtonStyle.red, label="Offline")
-    online = discord.ui.Button(style=discord.ButtonStyle.green, label="Online")
-    
-    embed=discord.Embed(title="Host", description = f"Host **{ip}** in progress ...",color=0x00FF00)
-    embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-    await ctx.channel.send(embed=embed)
+    await ctx.channel.send(embed=CompleteEmbed("Host", f"Host **{ip}** in progress ...", 0x00FF00))
     msg2= await ctx.channel.send(":hourglass:")
 
     try:
@@ -28,21 +22,14 @@ async def host (ctx,args):
 
     except TimeoutError:
         print("Timeout")
-        view.add_item(item=offline)
-        embed=discord.Embed(title="Timeout", description = f"➜ Timeout for **{ip}**",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        await ctx.channel.send(embed=embed, view=view)
+        await ctx.channel.send(embed=CompleteEmbed("Timeout", f"➜ Timeout for **{ip}**", 0xFF0000), view=simple_view("offline"))
         return
     except ErrorDuringProcess as err:
         print(f"Error code: {err.code}")
         await msg2.delete()
-        embed=discord.Embed(title=f"Error {err.code}", description = f"➜ Host **{ip}** not found.",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        view.add_item(item=offline)
-        await ctx.channel.send(embed=embed, view=view)
+        await ctx.channel.send(embed=CompleteEmbed("Error {err.code}",f"➜ Host **{ip}** not found.",0xFF0000), view=simple_view("offline"))
         return
         
     else:
-        view.add_item(item=online)
-        await msg2.edit(f"```py\n{res}```",view=view)
+        await msg2.edit(f"```py\n{res}```",view=simple_view("online"))
         return
