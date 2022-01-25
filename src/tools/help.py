6e2e84455774b5f1aba6e12e29c12e4f1ddf1fd3
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 18:12:13 +0100

    fix (help embed): use embed function

--- src/tools/help.py
+++ src/tools/help.py
@@ -1,9 +1,6 @@
-import discord
-import subprocess
+from msilib.schema import CompLocator
+from src.embed import *
 
 async def help (ctx):
-    
-    embed=discord.Embed(title="Help", description = "You can find all the commands here. \n \n`.dig` \n`.host` \n`.ping` \n`.traceroute` \n`.traceroute6` \n`.whois`",color=0xFF8B00)
-    embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-    await ctx.channel.send(embed=embed)
+    await ctx.channel.send(embed=CompleteEmbed("Help","You can find all commands here. \n \n`.dig` \n`.host` \n`.ping` \n`.traceroute` \n`.traceroute6` \n`.whois`",0xFF8B00))
     return
