058ac9d93f482ee6d116b2832da02730ab2983de
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 17:48:11 +0100

    fix (traceroute6 embed): use embed function

--- src/tools/traceroute6.py
+++ src/tools/traceroute6.py
@@ -1,26 +1,19 @@
-import discord
 from src.process import *
 from src.error import *
+from src.embed import *
+from src.view import *
 
 async def traceroute6 (ctx,args):
     
     if len(args) != 1 :
         print("Missing Required Argument")
-        embed=discord.Embed(title="Error", description = "Missing Required Argument. \n \n You should use the **traceroute** command with domain or IPv6 like this : \n \n Domain: \n`.traceroute google.com` \n IPv6: \n`.traceroute 2606:4700:4700::1111`",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        await ctx.channel.send(embed=embed)
+        await ctx.channel.send(embed=CompleteEmbed("Error","Missing Required Argument. \n \n You should use the **traceroute** command with domain or IPv6 like this : \n \n Domain: \n`.traceroute google.com` \n IPv6: \n`.traceroute 2606:4700:4700::1111`",0xFF0000))
         return
     
     ip=args[0]
     print("Traceroute6 for", ip, "request by", ctx.author.name,f"(ID = {ctx.author.id}).")
-
-    view = discord.ui.View()    
-    offline = discord.ui.Button(style=discord.ButtonStyle.red, label="Offline")
-    online = discord.ui.Button(style=discord.ButtonStyle.green, label="Online")
     
-    embed=discord.Embed(title="Traceroute6", description = f"Traceroute6 **{ip}** in progress ...",color=0x00FF00)
-    embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-    await ctx.channel.send(embed=embed)
+    await ctx.channel.send(embed=CompleteEmbed("Traceroute6", f"Traceroute6 **{ip}** in progress ...", 0x00FF00))
     msg2= await ctx.channel.send(":hourglass:")
 
     try:
@@ -28,19 +21,12 @@ async def traceroute6 (ctx,args):
 
     except TimeoutError:
         print("Timeout")
-        view.add_item(item=offline)
-        embed=discord.Embed(title="Timeout", description = f"➜ Timeout for **{ip}**",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        await ctx.channel.send(embed=embed, view=view)
+        await ctx.channel.send(embed=CompleteEmbed("Timeout", f"➜ Timeout for **{ip}**",color=0xFF0000), view=simple_view("offline"))
         return
     except ErrorDuringProcess as err:
         print(f"Error code: {err.code}")
         await msg2.delete()
-        embed=discord.Embed(title="Error", description = f":warning: **Error {err.code}** occured during process for **{ip}** :warning:",color=0xFF0000)
-        embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-        await ctx.channel.send(embed=embed, view=view)
-        view.add_item(item=offline)
+        await ctx.channel.send(embed=CompleteEmbed("Error", f":warning: **Error {err.code}** occured during process for **{ip}** :warning:",0xFF0000), view=simple_view("offline"))
         return
     else:
-        view.add_item(item=online)
-    await msg2.edit(view=view)
+        await msg2.edit(view=simple_view("online"))
