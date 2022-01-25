88d7e838d52b53e447d1e3f9ae7973f05aba664b
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 18:15:41 +0100

    fix (embed): change footer

--- src/embed.py
+++ src/embed.py
@@ -8,6 +8,5 @@ def BasicEmbed(title=None, description=None, color=None):
 def CompleteEmbed(title=None, description=None, color=None):
     embed = discord.Embed(title=title, description=description, color=color)
     embed.set_thumbnail(url="https://api.alexandregliganic.fr/folder/serveur.png")
-    embed.set_footer(text="By Alexandre Gliganic",
-                     icon_url="https://api.alexandregliganic.fr/folder/pp-alex2.jpg")
+    embed.set_footer(text="By Alexandre Gliganic", icon_url="https://api.alexandregliganic.fr/folder/pp-alex2.jpg")
     return embed
