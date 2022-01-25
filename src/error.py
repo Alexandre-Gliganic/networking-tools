709d3772476e5101e5d4f9ad4e9b1377f98b6b67
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 19:16:48 +0100

    fix (error refacto): remove comments

--- src/error.py
+++ src/error.py
@@ -2,5 +2,4 @@ class ErrorDuringProcess(Exception):
     def __init__(self,code: int,err: str, *args: object) -> None:
         self.code = code
         self.err = err
-        #print(*args)
         super().__init__("Process exited with code " + str(code))
