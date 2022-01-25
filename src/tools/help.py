3096e78fcac5d04533e946b8dd7cdf7b35c0d791
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-25 19:20:40 +0100

    fix (help refacto): split long lines

--- src/tools/help.py
+++ src/tools/help.py
@@ -1,5 +1,6 @@
 from src.embed import *
 
 async def help (ctx):
-    await ctx.channel.send(embed=CompleteEmbed("Help","You can find all commands here. \n \n`.dig` \n`.host` \n`.ping` \n`.traceroute` \n`.traceroute6` \n`.whois`",0xFF8B00))
+    await ctx.channel.send(embed=CompleteEmbed("Help","You can find all commands here\
+        \n \n`.dig` \n`.host` \n`.ping` \n`.traceroute` \n`.traceroute6` \n`.whois`",0xFF8B00))
     return
