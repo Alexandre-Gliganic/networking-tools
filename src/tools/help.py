ae03ce7b5fdd289e682ba413c69a676eadbd7a7f
Alexandre Gliganic <alexandre.gliganic@outlook.com>
2022-01-26 13:59:28 +0100

    feat (help ping6 function): add ping6 function

--- src/tools/help.py
+++ src/tools/help.py
@@ -2,5 +2,5 @@ from src.embed import *
 
 async def help (ctx):
     await ctx.channel.send(embed=CompleteEmbed("Help","You can find all commands here\
-        \n \n`.dig` \n`.host` \n`.ping` \n`.traceroute` \n`.traceroute6` \n`.whois`",0xFF8B00))
+        \n \n`.dig` \n`.host` \n`.ping` \n`.ping6` \n`.traceroute` \n`.traceroute6` \n`.whois`",0xFF8B00))
     return
